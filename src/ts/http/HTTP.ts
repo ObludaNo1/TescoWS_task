49ac02ecb7f12caa1aef52c4d6924903d025bab1
Ondřej Caha <ondrej.caha@windy.com>
2022-10-04 10:18:19 +0200

    Enhanced HTTP request

--- src/ts/http/HTTP.ts
+++ src/ts/http/HTTP.ts
@@ -1,5 +1,25 @@
+/**
+ * Class for HTTP requests
+ */
 export class HTTP {
-    get<T>(url: string): Promise<T> {
+    /**
+     * Get HTTP request as promise
+     * @param url Request URL
+     * @param urlParams aditional parameters into URL
+     * @returns HTTP request promise resolving when returned status code is 200, failing otherwise
+     */
+    get<T>(url: string, urlParams?: Record<string, string>): Promise<T> {
+        // ULR with aditional params
+        const urlWithParams =
+            url +
+            (urlParams
+                ? "?" +
+                  Object.entries(urlParams)
+                      .map((key, value) => `${key}=${value}`)
+                      .join("&")
+                : "");
+
+        // return actual request as promise
         return new Promise<T>((resolve, reject) => {
             const request = new XMLHttpRequest();
             request.onload = function () {
@@ -12,7 +32,7 @@ export class HTTP {
             request.onerror = function () {
                 reject(new Error("XMLHttpRequest Error: " + this.statusText));
             };
-            request.open("GET", url);
+            request.open("GET", urlWithParams);
             request.send();
         });
     }
