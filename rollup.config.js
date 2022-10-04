afb0bff4f4dbac4a32c19fc237e173a158080bd5
Ondřej Caha <ondrej.caha@windy.com>
2022-10-04 10:00:54 +0200

    Added rollup bundling

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default [
+    {
+        input: "src/ts/app.ts",
+        output: {
+            dir: "public/js",
+            format: "esm",
+            sourcemap: true,
+        },
+        plugins: [
+            typescript({
+                tsconfig: "./tsconfig.json",
+            }),
+        ],
+    },
+];
